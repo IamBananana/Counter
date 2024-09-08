87ea1eff626a892b948eea77a8dec81a84723bcf
IamBananana <richardgjelland@hotmail.com>
2024-09-08 14:21:09 +0200

    Added comments

--- index.js
+++ index.js
@@ -4,13 +4,22 @@
 //window.alert("This is an alert!");
 //window.alert("I like pizza");
 
-
+/**
+ * @author Richie
+ * Constants buttons are always going to be linked, hence immutable
+ */
 const increaseBtn = document.getElementById("increaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const decreaseBtn = document.getElementById("decreaseBtn");
 const countLabel = document.getElementById("countLabel");
+
+//Use let so that count is mutable(changeable)
 let count = 0;
 
+/**
+ * when a function is called onclick it updates the count and then
+ * changes the countLabel (<Label> html) to display the new count
+ */
 increaseBtn.onclick = function(){
     count++;
     countLabel.textContent = count;
